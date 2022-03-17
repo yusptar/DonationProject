3bf6d686b5ce329112c8371f690fb4e818460852
Firmanrachmad <dragobniel@gmail.com>
2022-03-17 10:41:18 +0700

    Tes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
       <div class="container">
 
         <div class="section-title">
-          <h2>About Us</h2>
+          <h2>About Us Test</h2>
           <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
 
