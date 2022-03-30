e0cbf60b73287e5614557f30fda27f4acb8610bd
Firmanrachmad <dragobniel@gmail.com>
2022-03-31 06:36:07 +0700

    add admin seeder

---
+++ database/seeders/AdministratorSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class AdministratorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new \App\Models\User;
+        $admin->name = "administrator";
+        $admin->email = "admin@gmail.com";
+        $admin->password = \Hash::make("admin123");
+        $admin->roles = "A";
+        $admin->save();
+        $this->command->info("Admin berhasil ditambah");
+    }
+}
