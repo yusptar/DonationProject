38c96a4c72583fc33f79e70e6ecd82c5aaf02e21
Firmanrachmad <dragobniel@gmail.com>
2022-03-31 06:58:12 +0700

    update icon dropdown

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -82,8 +82,8 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <li class="dropdown">
+                                <a id="navbarDropdown" class="dropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
