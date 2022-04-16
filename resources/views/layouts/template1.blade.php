1c3de14b0cd72441699eadfbdf692ba32595706b
Firmanrachmad <dragobniel@gmail.com>
2022-04-16 09:37:21 +0700

    update blade

--- resources/views/layouts/template1.blade.php
+++ resources/views/layouts/template1.blade.php
@@ -256,6 +256,9 @@
                                 href="{{ route('dashboard') }}" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span
                                     class="hide-menu">Dashboard</span></a></li>
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="tables.html" aria-expanded="false"><i class="mdi mdi-account-key"></i><span
+                                    class="hide-menu">Manage User</span></a></li>            
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                 href="charts.html" aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span
                                     class="hide-menu">Charts</span></a></li>
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
