81591d653a6468d21bbf4e8696cf9324e3d62db8
Firmanrachmad <dragobniel@gmail.com>
2022-05-11 09:50:07 +0700

    add new view

--- app/Http/Controllers/SantriController.php
+++ app/Http/Controllers/SantriController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Santri;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class SantriController extends Controller
 {
-    //
+    public function index() {
+		return view('manage_admin.santri_manage');
+	}
 }
