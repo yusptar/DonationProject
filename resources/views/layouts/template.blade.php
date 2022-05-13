811d4665e32baa15c1445eb760b50a8c9233f68c
yusptar <yusupvatar@gmail.com>
2022-05-13 09:08:48 +0700

    update ui&ux donatur pages

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -52,26 +52,27 @@
 
           <nav id="navbar" class="navbar order-last order-lg-0">
             <ul>
-              @can('admin')
+              <!-- @can('admin')
               <li><a class="nav-link scrollto" href="#">Data User</a></li>
               <li><a class="nav-link scrollto" href="#">Riwayat Donasi</a></li>
               <li><a class="nav-link scrollto" href="#">Cetak Laporan</a></li>
-              @endcan
-              
-
-              <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+              @endcan -->
+              @cannot('donatur')
+              <li><a class="nav-link scrollto active" href="#user">Home</a></li>
               <li><a class="nav-link scrollto" href="#about">Profil</a></li>
+              @endcannot
+
               @can('donatur')   
               <li><a class="nav-link scrollto" href="#services">Donasi</a></li>
               <li><a class="nav-link scrollto" href="#pricing">Riwayat Donasi</a></li>
               @endcan
 
               @cannot('donatur')
-              <li class="dropdown"><a href=""><span>Informasi</span><i class="bi bi-chevron-down"></i></a>
+              <li class="dropdown"><a href="#"><span>Informasi</span><i class="bi bi-chevron-down"></i></a>
                 <ul>
                   <li><a class="nav-link scrollto" href="#portfolio">Foto Kegiatan</a></li>
                   <li><a class="nav-link scrollto" href="#services">Berita</a></li>
-                  <li><a class="nav-link scrollto" href="#pricing">Donasi Sukarela</a></li>
+                  <li><a class="nav-link scrollto" href="#pricing">Donasi</a></li>
                 </ul>
               </li>
               <li><a class="nav-link scrollto" href="#contact">Hubungi Kami</a></li>
@@ -79,39 +80,40 @@
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
           </nav><!-- .navbar -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="get-started-btn scrollto" href="{{ route('login') }}">{{ __('Masuk/Daftar') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="dropdown">
-                                <a id="navbarDropdown" class="dropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest          
+          @guest
+            @if (Route::has('login'))
+              <li class="nav-item">
+                  <a class="get-started-btn scrollto" href="{{ route('login') }}">{{ __('Masuk/Daftar') }}</a>
+              </li>
+            @endif
+            @else
+              <nav id="navbar" class="navbar order-last order-lg-0">
+                <ul>
+                  <li class="dropdown">
+                      <a href="#" role="button" data-bs-toggle="dropdown">
+                          <span>{{ Auth::user()->name }}</span><i class="bi bi-chevron-down"></i>
+                      </a>
+                      <ul>       
+                          <li><a class="nav-link scrollto" href="#">Profile Saya</a></li>
+                          <li><a class="nav-link scrollto" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> {{ __('Logout') }}</a>
+                              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                              </form>
+                          </li>
+                      </ul>
+                  </li>
+          @endguest  
+                </ul>
+                    <i class="bi bi-list mobile-nav-toggle"></i>
+              </nav><!-- .navbar -->                       
         </div>
       </div>
-
     </div>
   </header><!-- End Header -->
 
-  <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex flex-column justify-content-center">
+  <!-- ======= Hero Section For Another User ======= -->
+  @cannot('donatur')
+  <section id="user" class="d-flex flex-column justify-content-center">
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-xl-8">
@@ -121,7 +123,25 @@
         </div>
       </div>
     </div>
-  </section><!-- End Hero -->
+  </section>
+  @endcannot
+  <!-- End Hero For Another User -->
+  
+
+  @can('donatur')
+  <!-- ======= Hero Section For Donatur ======= -->
+  <section id="donatur" class="d-flex flex-column justify-content-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xl-8">
+          <h1>Selamat Datang, {{ Auth::user()->name }}</h1>
+          <h2>Bersama kita merajut masa depan Anak Yatim</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+  @endcan
+  <!-- End Hero For Donatur -->
 
   <main id="main">
    @yield('content')
