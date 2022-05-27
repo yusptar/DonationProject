b5ea9395d14748da67d9bcd467e608d64fdbe3a4
Firmanrachmad <dragobniel@gmail.com>
2022-05-27 10:48:22 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -75,6 +75,22 @@ Route::delete('/santri-delete', [SantriController::class, 'delete'])->name('sant
 Route::get('/santri-edit', [SantriController::class, 'edit'])->name('santri-edit');
 Route::post('/santri-update', [SantriController::class, 'update'])->name('santri-update');
 
+// Manage Donatur
+Route::get('/donatur', [DonaturController::class, 'index']);
+Route::post('/donatur-store', [DonaturController::class, 'store'])->name('donatur-store');
+Route::get('/donatur-fetchall', [DonaturController::class, 'fetchAll'])->name('donatur-fetchAll');
+Route::delete('/donatur-delete', [DonaturController::class, 'delete'])->name('donatur-delete');
+Route::get('/donatur-edit', [DonaturController::class, 'edit'])->name('donatur-edit');
+Route::post('/donatur-update', [DonaturController::class, 'update'])->name('donatur-update');
+
+// Manage Pnegasuh
+Route::get('/pengasuh', [PengasuhController::class, 'index-table']);
+Route::post('/pengasuh-store', [PengasuhController::class, 'store'])->name('donatur-store');
+Route::get('/pengasuh-fetchall', [PengasuhController::class, 'fetchAll'])->name('donatur-fetchAll');
+Route::delete('/pengasuh-delete', [PengasuhController::class, 'delete'])->name('donatur-delete');
+Route::get('/pengasuh-edit', [PengasuhController::class, 'edit'])->name('donatur-edit');
+Route::post('/pengasuh-update', [PengasuhController::class, 'update'])->name('donatur-update');
+
 
 
 
