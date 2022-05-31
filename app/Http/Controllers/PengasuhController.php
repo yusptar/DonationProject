e7d9baa76c4ea8b56b56bf77a4ceb5f5fd2c85e3
Firmanrachmad <dragobniel@gmail.com>
2022-06-01 05:43:50 +0700

    update

--- app/Http/Controllers/PengasuhController.php
+++ app/Http/Controllers/PengasuhController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Hash;
 
 class PengasuhController extends Controller
 {
@@ -15,4 +18,50 @@ class PengasuhController extends Controller
     {
         return view('pengasuh.pengasuh');
     }
+    public function index_table()
+    {
+        return view('manage_admin.pengasuh_manage');
+    }
+    public function fetchAll() {
+		$emps = User::latest()->where('roles', 'Pengasuh');
+		$output = '';
+		$numbering = 1;
+		if ($emps->count() > 0) {
+			$output .= '<table class="table table-bordered data-table">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Foto</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Alamat</th>
+                <th>No. Hp</th>
+                <th>Instansi</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>';
+			foreach ($emps as $emp) {
+				$output .= '<tr>
+                <td width="5px">' . $numbering++ . '</td>
+                <td><img src="storage/images/' . $emp->image . '" width="100" height="100" src="https://via.placeholder.com/150"></td>
+                <td>' . $emp->name . '</td>
+                <td>' . $emp->email . '</td>
+                <td>' . $emp->alamat . '</td>
+                <td>' . $emp->nohp . '</td>
+                <td>' . $emp->instansi . '</td>
+                <td>
+                  <a href="#" id="' . $emp->id . '" class="edit btn btn-primary btn-sm editIcon" data-bs-toggle="modal" data-bs-target="#editPengasuhModal"><i class="bi-pencil-square h4"></i>Edit</a>
+
+                  <a href="#" id="' . $emp->id . '" class="btn btn-danger btn-sm deleteIcon"><i class="bi-trash h4"></i>Delete</a>
+
+                </td>
+              </tr>';
+			}
+			$output .= '</tbody></table>';
+			echo $output;
+		} else {
+			echo '<h1 class="text-center text-secondary my-5">No record present in the database!</h1>';
+		}
+	}
 }
--- resources/views/layouts/template1.blade.php
+++ resources/views/layouts/template1.blade.php
@@ -279,15 +279,16 @@
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                 href="#" aria-expanded="false"><i class="mdi mdi-account-card-details"></i><span
                                     class="hide-menu">Data Donatur</span></a></li>
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                href="#" aria-expanded="false"><i class="mdi mdi-receipt"></i><span
-                                    class="hide-menu">Donasi Offline</span></a></li>
+                        
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                 href="#" aria-expanded="false"><i class="mdi mdi-clipboard-flow"></i><span
                                     class="hide-menu">Data Masuk Donasi</span></a></li>                
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                 href="#" aria-expanded="false"><i class="mdi mdi-printer"></i><span
-                                    class="hide-menu">Cetak Laporan</span></a></li>                                                                             
+                                    class="hide-menu">Cetak Laporan</span></a></li>  
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="#" aria-expanded="false"><i class="mdi mdi-receipt"></i><span
+                                    class="hide-menu">Donasi Offline</span></a></li>                                                                           
                         <!-- <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                 href="charts.html" aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span
                                     class="hide-menu">Charts</span></a></li>
