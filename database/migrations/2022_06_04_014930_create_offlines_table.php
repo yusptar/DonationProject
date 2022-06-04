2f78a61aaddbf5683f47c4ee7ea50a81dfb72376
Firmanrachmad <dragobniel@gmail.com>
2022-06-05 06:24:22 +0700

    update NEW table

---
+++ database/migrations/2022_06_04_014930_create_offlines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOfflinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offlines', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->integer('nominal');
+            $table->string('doa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offlines');
+    }
+}
