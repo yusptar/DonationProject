f562784fc44b1bd045d88965f53cff9abad5c1ba
yusptar <yusupvatar@gmail.com>
2022-06-13 10:43:20 +0700

    update new ui&ux profile section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,37 +1,58 @@
 @extends('layouts.template')
 @section('content')
-    <style>
-      .total-donasi{
-        font-weight: bold;
-        font-size: 15px;
-      }
-
-      .jumlah-donatur{
-        font-weight: bold;
-        font-size: 15px;
-      }
-    </style>
+    <br>
     <!-- ======= Profile Section ======= -->
-    <section id="about" class="about">
-      <div class="container">
-        <div class="section-title">   
-          <h2>Profil</h2><a class="logo me-auto me-lg-0"><img src="{{ asset('user/assets/img/logotext.png') }}" alt="" class="img-fluid"></a>
-          <p>Yayasan Attaufiq adalah Yayasan yang fokus mengabdi untuk mengasuh anak Yatim Piatu dan fakir miskin yang berada di Malang. Dengan segala kemampuan, kami bertanggungjawab mewujudkan cita cita Anak Yatim agar bisa terwujud kelak hingga mereka dewasa.</p>
-        </div>
-        <div class="row content">
-          <div class="col-lg-6">
-            <p>
-            Bermula dari acara pengajian yang diadakan secara sederhana dalam rangka syukuran peringatan HUT Kemerdekaan RI pada bulan Agustus 1995 para peserta pengajian sepakat dan bertekad untuk memulai merawat anak yatim dengan modal uang hasil spontanitas sebesar Rp. 55.000,-. Dua bulan berikutnya uang tersebut bertambah menjadi Rp. 70.000,- dan dua bulan berikutnya lagi, tepatnya pada bulan November 1995 menjadi 125.000,- setelah diadakan pertemuan pertama untuk memastikan perlu tidaknya realisasi kesepakatan dan tekad di atas.<br>
-            <br>Menyadari betapa sudah sangat jelas pesan agama Islam bahwa persoalan anak yatim dan orang miskin adalah menjadi tanggung jawab umat Islam yang dengan berkat Rahmat Allah swt. mereka mempunyai kelonggaran atau kelebihan untuk ikut serta secara aktif menyelesaikan problem tersebut agar masa depan mereka bisa survive, maka perlu segera diadakan musyawarah untuk membicarakan kesepakatan dan tekad yang sangat mulia dan terpuji tersebut.
-            </p>
+    <section id="profile" class="profile">
+      <div class="container-fluid">
+
+        <div class="row">
+
+          <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
+
+            <div class="content">
+              <a class="logo me-auto me-lg-0"><img src="{{ asset('user/assets/img/logotext.png') }}" alt="" class="img-fluid"></a>
+              <p></p>
+            </div>
+
+            <div class="accordion-list">
+              <ul>
+                <li>
+                  <a data-bs-toggle="collapse" class="collapse" data-bs-target="#accordion-list-1">Profile Yayasan At-Taufiq Malang<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                  <div id="accordion-list-1" class="collapse show" data-bs-parent=".accordion-list">
+                    <p>
+                    Yayasan Attaufiq adalah Yayasan yang fokus mengabdi untuk mengasuh anak Yatim Piatu dan fakir miskin yang berada di Malang. Dengan segala kemampuan, kami bertanggungjawab mewujudkan cita cita Anak Yatim agar bisa terwujud kelak hingga mereka dewasa.
+                    </p>
+                  </div>
+                </li>
+
+                <li>
+                  <a data-bs-toggle="collapse" data-bs-target="#accordion-list-2" class="collapsed">Sejarah Berdirinya Yayasan At-Taufiq Malang<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                  <div id="accordion-list-2" class="collapse" data-bs-parent=".accordion-list">
+                    <p>
+                    Bermula dari acara pengajian yang diadakan secara sederhana dalam rangka syukuran peringatan HUT Kemerdekaan RI pada bulan Agustus 1995 para peserta pengajian sepakat dan bertekad untuk memulai merawat anak yatim dengan modal uang hasil spontanitas sebesar Rp. 55.000,-. Dua bulan berikutnya uang tersebut bertambah menjadi Rp. 70.000,- dan dua bulan berikutnya lagi, tepatnya pada bulan November 1995 menjadi 125.000,- setelah diadakan pertemuan pertama untuk memastikan perlu tidaknya realisasi kesepakatan dan tekad di atas.<br>
+                    <br>Menyadari betapa sudah sangat jelas pesan agama Islam bahwa persoalan anak yatim dan orang miskin adalah menjadi tanggung jawab umat Islam yang dengan berkat Rahmat Allah swt. mereka mempunyai kelonggaran atau kelebihan untuk ikut serta secara aktif menyelesaikan problem tersebut agar masa depan mereka bisa survive, maka perlu segera diadakan musyawarah untuk membicarakan kesepakatan dan tekad yang sangat mulia dan terpuji tersebut.
+                    </p>
+                  </div>
+                </li>
+
+                <li>
+                  <a data-bs-toggle="collapse" data-bs-target="#accordion-list-3" class="collapsed">Lanjutan Sejarah Berdirinya Yayasan At-Taufiq Malang <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                  <div id="accordion-list-3" class="collapse" data-bs-parent=".accordion-list">
+                    <p>
+                    Yayasan Anak Yatim AT-TAUFIQ secara resmi berdiri pada tanggal 4 April 1996 berdasarkan dokumen akte notaris nomor 30 tahun 1996 dan Surat Tanda Pendaftaran (STP) yang dikeluarkan oleh Departemen Sosial Kantor Wilayah Jawa Timur nomor 450/STP/ORSOS/XI/1996 bergerak di bidang sosial keagamaan.<br>
+                    <br>Sejalan dengan perkembangannya, Yayasan kali ini sudah memiliki dua unit bangunan, yaitu satu unit asrama dan satu unit masjid yang dibangun di atas tanah wakaf dengan luas 540 m2 yang diperoleh pada tahun 1997 dari keluarga almarhum Bapak H. Abdurrahman Sanan Malang. Asrama tersebut baru bisa menampung anak asuh putra, dan untuk pengembangannya insyaallah akan dibangun satu unit lagi asrama yang dapat menampung anak asuh putri, yang sementara masih bertempat tinggal di rumah keluarga masing-masing.
+                    </p>
+                  </div>
+                </li>
+
+              </ul>
+            </div>
+
           </div>
-          <div class="col-lg-6 pt-4 pt-lg-0">
-            <p>
-            Yayasan Anak Yatim AT-TAUFIQ secara resmi berdiri pada tanggal 4 April 1996 berdasarkan dokumen akte notaris nomor 30 tahun 1996 dan Surat Tanda Pendaftaran (STP) yang dikeluarkan oleh Departemen Sosial Kantor Wilayah Jawa Timur nomor 450/STP/ORSOS/XI/1996 bergerak di bidang sosial keagamaan.<br>
-            <br>Sejalan dengan perkembangannya, Yayasan kali ini sudah memiliki dua unit bangunan, yaitu satu unit asrama dan satu unit masjid yang dibangun di atas tanah wakaf dengan luas 540 m2 yang diperoleh pada tahun 1997 dari keluarga almarhum Bapak H. Abdurrahman Sanan Malang. Asrama tersebut baru bisa menampung anak asuh putra, dan untuk pengembangannya insyaallah akan dibangun satu unit lagi asrama yang dapat menampung anak asuh putri, yang sementara masih bertempat tinggal di rumah keluarga masing-masing.
-            </p>
-          </div>
+
+          <div class="col-lg-5 align-items-stretch order-1 order-lg-2 img" style='background-image: url("user/assets/img/yayasan-building.jpeg");'>&nbsp;</div>
         </div>
+
       </div>
     </section><!-- End Profile Section -->
 
