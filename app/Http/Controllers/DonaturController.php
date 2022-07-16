a5fd79deda7be3676c7be1691ae7021726df707a
yusptar <yusupvatar@gmail.com>
2022-07-16 20:02:17 +0700

    latest update donatur controller

--- app/Http/Controllers/DonaturController.php
+++ app/Http/Controllers/DonaturController.php
@@ -12,171 +12,126 @@ class DonaturController extends Controller
 {
     // ----------------------- DONATUR PAGE ------------------------ //
 
-    public function index(){
-        $user = User::all();
-        $donasi = Donation::latest()->paginate(5);
-        return view('donatur.donatur', ['user' => $user, 'donasi' => $donasi]);
+   // Donatur Page View Function
+
+   public function index(){
+    $user = User::all();
+    $donasi = Donation::latest()->paginate(5);
+    return view('donatur.donatur', ['user' => $user, 'donasi' => $donasi]);
+  }
+
+  // Riwayat Donasi View Function 
+
+  public function list_donasi(Request $request)
+  {
+      $donasi = Donation::where('donatur_id', Auth::user()->id)->first();
+      $data_donasi = Donation::latest()->paginate(4)->where('donatur_id', Auth::user()->id);
+      $jumlah_donasi_berhasil = Donation::latest()->where('donatur_id', '=', Auth::user()->id)
+                                        ->where('status', 'settlement')->count();
+      $jumlah_donasi_pending = Donation::latest()->where('donatur_id', '=', Auth::user()->id)
+                                        ->where('status', 'pending')->count();
+      return view('donatur.donasi', [
+          'donasi' => $donasi, 
+          'data_donasi' => $data_donasi,
+          'jumlah_donasi_berhasil' => $jumlah_donasi_berhasil,
+          'jumlah_donasi_pending' => $jumlah_donasi_pending
+      ]);
+  }
+
+  // Snap Payment Function
+
+  public function payment(Request $request)
+  {
+    $user = User::where('id', Auth::user()->id)->first();
+
+    // Set your Merchant Server Key
+    \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+    // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+    \Midtrans\Config::$isProduction = false;
+    // Set sanitization on (default)
+    \Midtrans\Config::$isSanitized = true;
+    // Set 3DS transaction for credit card to true
+    \Midtrans\Config::$is3ds = true;
+
+    $params = array(
+      'transaction_details' => array(
+          'order_id' => 'OD-'.date('Ymd').rand(1111,9999),
+          'gross_amount' => $request->get('nominal'),
+      ),
+      'customer_details' => array(
+          'first_name' => $request->get('donatur_name'),
+          'last_name' => '',
+          'email' => $request->get('donatur_email'),
+          'phone' => $request->get('donatur_phone'),
+      ),
+    );
+
+    $snapToken = \Midtrans\Snap::getSnapToken($params);
+    return view('donatur.payment', [ 'snap_token' => $snapToken]);
+  }
+
+  // Post Data to Database Payment Function
+
+  public function payment_post(Request $request){
+    $isNameHidden = false;
+    $user = User::where('id', Auth::user()->id)->first();
+    $donasi = Donation::where('donatur_id', Auth::user()->id)->first();
+    if ($request->isNameHidden == 'on') {
+      $isNameHidden = true;
     }
+    $json = json_decode($request->get('json'));
+    $donasi = new Donation();
+    
+    $donasi->donatur_id = Auth::user()->id;
+    $donasi->status = $json->transaction_status;
+    $donasi->isNameHidden = $isNameHidden;
+    $donasi->donatur_name = $request->get('donatur_name');
+    $donasi->donatur_email = $request->get('donatur_email');
+    $donasi->nominal = $request->get('nominal');
+    $donasi->message = $request->get('message');
+    $donasi->image = Auth::user()->image;
+    $donasi->donatur_phone = $request->get('donatur_phone');
+    $donasi->transaction_id = $json->transaction_id;
+    $donasi->order_id = $json->order_id;
+    $donasi->gross_amount = $json->gross_amount;
+    $donasi->payment_type = $json->payment_type;
+    $donasi->payment_code = isset($json->payment_code) ? $json->payment_code : null;
+    $donasi->pdf_url = isset($json->pdf_url) ? $json->pdf_url : null;
 
-
-    public function payment(Request $request)
-    {
-        $user = User::where('id', Auth::user()->id)->first();
-
-        // Set your Merchant Server Key
-        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
-        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        \Midtrans\Config::$isProduction = false;
-        // Set sanitization on (default)
-        \Midtrans\Config::$isSanitized = true;
-        // Set 3DS transaction for credit card to true
-        \Midtrans\Config::$is3ds = true;
-
-        $params = array(
-          'transaction_details' => array(
-              'order_id' => rand(),
-              'gross_amount' => $request->get('nominal'),
-          ),
-          'customer_details' => array(
-              'first_name' => $request->get('donatur_name'),
-              'last_name' => '',
-              'email' => $request->get('donatur_email'),
-              'phone' => $request->get('donatur_phone'),
-          ),
-        );
-
-        $snapToken = \Midtrans\Snap::getSnapToken($params);
-        return view('donatur.payment', [ 'snap_token' => $snapToken]);
-    }
-
-    public function payment_post(Request $request){
-        $isNameHidden = false;
-        $user = User::where('id', Auth::user()->id)->first();
-        $donasi = Donation::where('donatur_id', Auth::user()->id)->first();
-        if ($request->isNameHidden == 'on') {
-          $isNameHidden = true;
-        }
-        $json = json_decode($request->get('json'));
-        $donasi = new Donation();
-        
-        $donasi->donatur_id = Auth::user()->id;
-        $donasi->status = $json->transaction_status;
-        $donasi->isNameHidden = $isNameHidden;
-        $donasi->donatur_name = $request->get('donatur_name');
-        $donasi->donatur_email = $request->get('donatur_email');
-        $donasi->nominal = $request->get('nominal');
-        $donasi->message = $request->get('message');
-        $donasi->image = Auth::user()->image;
-        $donasi->donatur_phone = $request->get('donatur_phone');
-        $donasi->transaction_id = $json->transaction_id;
-        $donasi->order_id = $json->order_id;
-        $donasi->gross_amount = $json->gross_amount;
-        $donasi->payment_type = $json->payment_type;
-        $donasi->payment_code = isset($json->payment_code) ? $json->payment_code : null;
-        $donasi->pdf_url = isset($json->pdf_url) ? $json->pdf_url : null;
-
-        $donasi->save();
-
-        Alert::success('Transaksi Berhasil Dibuat<br>شُكْرًا');
-        return redirect()->back();
-    }
-
-    public function pending_payment(Request $request)
-    {
-        $donasi = Donation::where('donatur_id', Auth::user()->id)->first();
-        $data_donasi = Donation::latest()->paginate(1)->where('donatur_id', Auth::user()->id);
-        $jumlah_donasi = Donation::latest()->where('donatur_id', Auth::user()->id)->count();
-
-        // Set your Merchant Server Key
-        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
-        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        \Midtrans\Config::$isProduction = false;
-        // Set sanitization on (default)
-        \Midtrans\Config::$isSanitized = true;
-        // Set 3DS transaction for credit card to true
-        \Midtrans\Config::$is3ds = true;
-
-        if(!empty($donasi)){
-          $params = array(
-            'transaction_details' => array(
-                'order_id' => rand(),
-                'gross_amount' => $donasi->gross_amount,
-            ),
-            'customer_details' => array(
-                'first_name' => $donasi->donatur_name,
-                'last_name' => '',
-                'email' => $donasi->donatur_email,
-                'phone' => $donasi->donatur_phone,
-            ),
-          );
-        }else{
-          $params = array(
-            'transaction_details' => array(
-                'order_id' => rand(),
-                'gross_amount' => 1000,
-            ),
-            'customer_details' => array(
-                'first_name' => 'Tidak Dikenal',
-                'last_name' => '',
-                'email' => 'example@gmail.com',
-                'phone' => '0341240404',
-            ),
-          );
-        }
-
-
-        $snapToken = \Midtrans\Snap::getSnapToken($params);
-        return view('donatur.donasi', [ 'data_donasi' => $data_donasi, 
-                                        'jumlah_donasi' => $jumlah_donasi, 
-                                        'snap_token' => $snapToken
-                                      ]);
-    }
-
-    public function pending_payment_post(Request $request){
-        $isNameHidden = false;
-        $user = User::where('id', Auth::user()->id)->first();
-        $donasi = Donation::latest()->where('donatur_id', Auth::user()->id);
-        if ($request->isNameHidden == 'on') {
-          $isNameHidden = true;
-        }
-        $json = json_decode($request->get('json'));
-        $donasi = new Donation();
-        
-        $donasi->donatur_id = Auth::user()->id;
-        $donasi->status = $json->transaction_status;
-        $donasi->isNameHidden = $isNameHidden;
-        $donasi->donatur_name = $request->get('donatur_name');
-        $donasi->donatur_email = $request->get('donatur_email');
-        $donasi->nominal = $request->get('nominal');
-        $donasi->message = $request->get('message');
-        $donasi->donatur_phone = $request->get('donatur_phone');
-        $donasi->image = Auth::user()->image;
-        $donasi->donatur_phone = Auth::user()->nohp;
-        $donasi->transaction_id = $json->transaction_id;
-        $donasi->order_id = $json->order_id;
-        $donasi->gross_amount = $json->gross_amount;
-        $donasi->payment_type = $json->payment_type;
-        $donasi->payment_code = isset($json->payment_code) ? $json->payment_code : null;
-        $donasi->pdf_url = isset($json->pdf_url) ? $json->pdf_url : null;
-
-        $donasi->update();
-
-        Alert::success('Transaksi Berhasil<br>شُكْرًا');
-        return redirect()->back();
-    }
-
-    public function cancel_payment($id){
-        $donasi = Donation::where('id', $id)->delete();
-        Alert::success('Data berhasil dihapus');
-        return redirect()->back();
-    }
-
-    /*public function confirm_payment($id){
-      $donasi = Donation::where('id', $id)->delete();
-      Alert::success('Donasi berhasil dikonfirmasi');
+    $donasi->save();
+    
+    if ($donasi->status == 'settlement') {
+      Alert::success('Transaksi Berhasil', 'شُكْرًا');
       return redirect()->back();
-    }*/
+    }elseif($donasi->status == 'pending') {
+      Alert::info('Transaksi Pending');
+      return redirect()->back();
+    }
+   
+  }
 
+  // Webhooks of Payment Status Function 
+
+  public function confirmDonasi(Request $request){
+    $donasiPending = Donation::where('order_id', '=', $request['order_id'])->first();
+    $donasiEncryption = hash("sha512",$donasiPending->order_id.'200'.$donasiPending->gross_amount.env('MIDTRANS_SERVER_KEY'));
+    if ($request->signature_key == $donasiEncryption 
+        && $donasiPending->status == 'pending' 
+        && ($request->transaction_status == 'settlement' || $request->transaction_status == 'capture')) {
+        $donasiPending->status = 'settlement';
+        $donasiPending->payment_type = $request['payment_type'];
+        $donasiPending->save();
+    }
+    return $donasiPending;
+  }
+
+  // Cancel Payment Function
+
+  public function cancel_payment($id){
+    $donasi = Donation::where('id', $id)->delete();
+    Alert::success('Donasi berhasil dihapus');
+    return redirect()->back();
+  }
 
     // ------------------------- ADMIN ----------------------- //
 
