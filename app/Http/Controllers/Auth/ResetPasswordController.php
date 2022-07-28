b48fd080a6699c300d9d12e9c32e6bf1a82eb378
yusptar <yusupvatar@gmail.com>
2022-07-28 08:59:35 +0700

    update fitur lupa password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use App\Models\User;
 
 class ResetPasswordController extends Controller
 {
@@ -27,4 +28,15 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+    protected function redirectTo(){
+        if( Auth()->user()->roles == 'Admin' ){
+            return route('login');
+        }
+        elseif( Auth()->user()->roles == 'Donatur' ){
+            return route('login');
+        }
+        elseif( Auth()->user()->roles == 'Pengasuh'){
+            return route('login');
+        }
+    }
 }
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,85 +1,69 @@
 @extends('layouts.app')
-
 @section('content')
-<section class="sign-in">
-            <div class="container">
-                <div class="signin-content">
-                    <div class="signin-image">
-                        <a href="/"><img src="https://attaufiqmlg.com/wp-content/uploads/2016/06/logotext.png" alt="responsive"></a>
-                        <a href="{{ route('login') }}" class="signup-image-link">Kembali</a>
-                    </div>
+    <style>
+        a {
+            text-decoration: none;
+            display: inline-block;
+            padding: 8px 16px;
+            margin-top: 20px;
+            margin-left: 20px;
+            border-radius: 10px;
+            transition-duration: 0.4s;
+            cursor: pointer;
+        }
 
-                    <div class="card-body">
+        a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .previous {
+            background-color: #04AA6D;
+            color: white;
+        }
+
+        .image{
+            margin-left: 80px;
+            margin-right: 80px;
+            margin-top: 80px;
+            margin-bottom: 80px;
+        }
+    </style>
+
+    <section class="sign-in">
+        <div class="container">
+            <a href="{{ route('login') }}" class="previous">Kembali</a>
+            <div class="signin-content">
+                <div class="image">
+                    <a href="/"><img src="https://attaufiqmlg.com/wp-content/uploads/2016/06/logotext.png" alt="responsive"></a>
+                </div>
+
+                <div class="card-body">    
+                    <div class="signin-form">
+                        <h2 class="form-title">Reset Password</h2>
+                        <form method="POST" action="{{ route('password.email') }}">
+                        @csrf
+                        <div class="form-group">
+                            <label for="email"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Masukkan Email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
                             </div>
                         @endif
-
-                        <div class="signin-form">
-                            <h2 class="form-title">Reset Password</h2>
-                            <form method="POST" action="{{ route('password.email') }}">
-                            @csrf
-                            
-                                <div class="form-group">
-                                    <label for="email"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Masukkan Email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                                        @error('email')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                </div>
-                                <div class="form-group form-button">
-                                        <input type="submit" name="signin" id="signin" class="form-submit" value="Reset Password"></input>
-                                </div>
-                            </form>
-                            </div>`
+                        <div class="form-group form-button">
+                            <input type="submit" name="signin" id="signin" class="form-submit" value="Reset Password"></input>
                         </div>
+                        </form>
                     </div>
-                </div>
-            </section>
-<!-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
             </div>
         </div>
-    </div>
-</div> -->
+    </section>
 @endsection
